2896cd1398f0731e6f77bc2a141aca9210c23cbc
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-12-02 17:34:13 +0700

    app: update constants

--- app/containers/LogoutDialog/constants.js
+++ app/containers/LogoutDialog/constants.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const APP_STATE_NAME = 'logoutDialog';
-export const SHOW_LOGOUT_DIALOG = 'dicoapp/LogoutDialog/SHOW_LOGOUT_DIALOG';
-export const CANCEL_LOGOUT_DIALOG = 'dicoapp/LogoutDialog/CANCEL_LOGOUT_DIALOG';
-export const AGREE_LOGOUT_DIALOG = 'dicoapp/LogoutDialog/AGREE_LOGOUT_DIALOG';
+export const SHOW_LOGOUT_DIALOG = 'atomicapp/LogoutDialog/SHOW_LOGOUT_DIALOG';
+export const CANCEL_LOGOUT_DIALOG =
+  'atomicapp/LogoutDialog/CANCEL_LOGOUT_DIALOG';
+export const AGREE_LOGOUT_DIALOG = 'atomicapp/LogoutDialog/AGREE_LOGOUT_DIALOG';
